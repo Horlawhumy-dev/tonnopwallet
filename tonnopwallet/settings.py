2173ee34ec7a6eec61ea609715498bfee9fde4cf
Arafat Olayiwola <harof.dev@gmail.com>
2022-06-13 14:29:11 +0100

    allowed host

--- tonnopwallet/settings.py
+++ tonnopwallet/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'nameofapp.herokuapp.com']
 
 
 # Application definition
